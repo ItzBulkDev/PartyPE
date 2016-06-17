5346662dc2a1ced03cddfdd4af0ab5fd26b8d72e
Syntaxious <SynPlaysMC@gmail.com>
2016-06-18 00:51:16 +0100

    Update Task.php

--- src/PartyPE/Task.php
+++ src/PartyPE/Task.php
@@ -1,17 +1,1 @@
-<?php
 
-namespace PartyPE;
-
-use pocketmine\scheduler\PluginTask;
-use pocketmine\plugin\Plugin;
-
-use pocketmine\Player;
-use pocketmine\utils\TextFormat;
-
-class Task extends PluginTask{
-  
-	private $target;
-	private $requestplayer;
-	
-	public function __construct(Plugin $owner, Player $target,Player $requestplayer){
-	}
