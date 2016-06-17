8e1ebb30d773adfa1106d7d406b4dc2fb78d2fd8
Irish <mattisawesome223@gmail.com>
2016-06-17 19:23:08 -0400

    Update Main.php

--- src/PartyPE/Main.php
+++ src/PartyPE/Main.php
@@ -9,7 +9,7 @@ use pocketmine\utils\TextFormat as TF;
 class Main extends PluginBase implements Listener{
     
     public function onEnable(){
-        $this->getLogger()->info(TF::AQUA . "[PartyPE] enabled");
+        $this->getLogger()->notice("[PartyPE] enabled");//notice automatically does aqua I believe
         $this->getServer()->getPluginManager()->registerEvents($this,$this);
         @mkdir($this->getDataFolder());
     }
